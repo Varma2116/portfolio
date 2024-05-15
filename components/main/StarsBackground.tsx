64ed6ea49f2e53ad76e00190e17a09f9ad69a469
varaprasad <j.varaprasad@getshopse.com>
2024-05-15 12:58:56 +0530

    changes

--- components/main/StarsBackground.tsx
+++ components/main/StarsBackground.tsx
@@ -16,6 +16,7 @@ const StarBackground = (props: any) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   });
+  
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
