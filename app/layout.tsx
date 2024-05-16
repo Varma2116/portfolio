0b420822808d42e9af592eb1e9a4fc24cd6d62bc
varaprasad <j.varaprasad@getshopse.com>
2024-05-16 14:42:26 +0530

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,29 +7,38 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    "https://portfolio-vara-prasad.vercel.app/"
-  ),
+  metadataBase: new URL("https://portfolio-vara-prasad.vercel.app/"),
   title: "Jaladanki Venkata Vara Prasad Freelancer Portfolio",
   description: "Jaladanki Venkata Vara Prasad Freelancer Portfolio",
-  keywords: ["Developer", "Portfolio", "Developer Portflio", "JVVPRASAD", "JALADANKI VENKATA VARA PRASAD"],
+  keywords: [
+    "Developer", 
+    "Portfolio", 
+    "Developer Portfolio", 
+    "JVVPRASAD", 
+    "Jaladanki Venkata Vara Prasad"
+  ],
   openGraph: {
-    title: "JALADANKI VENKATA VARA PRASAD",
+    title: "Jaladanki Venkata Vara Prasad",
     description: "Software Engineer 2",
-    images: "/jvvprasad.jpeg",
+    images: "https://portfolio-vara-prasad.vercel.app/jvvprasad.jpeg",
+    url: "https://portfolio-vara-prasad.vercel.app/",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jaladanki Venkata Vara Prasad",
+    description: "Software Engineer 2",
+    images: "https://portfolio-vara-prasad.vercel.app/jvvprasad.jpeg",
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} bg-[#111] overflow-y-scroll overflow-x-hidden`}
-      >
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://portfolio-vara-prasad.vercel.app/" />
+      </head>
+      <body className={`${inter.className} bg-[#111] overflow-y-scroll overflow-x-hidden`}>
         <StarsCanvas />
         <Navbar />
         {children}
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://portfolio-vara-prasad.vercel.app',
+    generateRobotsTxt: true,
+  };
+  
